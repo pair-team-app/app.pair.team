9890438fd6cc7f57d1cf9da00699a72ef7f01df1
Matt H <matt@designengine.ai>
2018-10-11 19:24:11 -0700

    render page first pass

--- src/components/pages/InspectorPage.jsx
+++ src/components/pages/InspectorPage.jsx
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import './InspectorPage.css';
 
+// import axios from 'axios';
 // import { Column, Row } from 'simple-flexbox';
 
 
@@ -22,7 +23,49 @@ class InspectorPage extends Component {
 	render() {
 		return (
 			<div className="inspector-page-wrapper">
-				INSPECTOR PAGE ({this.state.itemID})
+				<div className="inspector-page-content">
+					<div className="inspector-page-hero-wrapper">
+						<img className="inspector-page-hero-image" src="" alt="Hero" />
+						<div className="inspector-page-hero-title">
+							Material Design System
+						</div>
+					</div>
+					<div className="inspector-page-hero-info-wrapper">
+						0 Views<br />
+						0 Downloads
+					</div>
+				</div>
+				<div className="inspector-page-panel">
+					<div className="inspector-page-panel-display">
+					</div>
+					<div className="inspector-page-panel-button-wrapper">
+						<div>
+							<button className="inspector-page-size-button">1x</button>
+							<button className="inspector-page-size-button inspector-page-size-button-middle">2x</button>
+							<button className="inspector-page-size-button">3x</button>
+						</div>
+						<div>
+							<button className="inspector-page-select-button">Select Part ()</button>
+							<button className="inspector-page-download-button">Download All ()</button>
+						</div>
+					</div>
+					<div className="inspector-page-panel-info-wrapper">
+						System: <br />
+						Author: <br />
+						Page: <br />
+						Artboard: <br />
+						Slice: <br />
+						Position: <br />
+						Size: <br />
+						Rotation: <br />
+						Opacity: <br />
+						Color: <br />
+						Font: <br />
+						Font Size: <br />
+						Font Color: <br />
+						Blend Mode: <br />
+					</div>
+				</div>
 			</div>
 		);
 	}
