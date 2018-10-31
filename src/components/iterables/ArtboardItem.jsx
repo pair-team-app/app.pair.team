aaf313a2e14a24790c3f862d9a21498b2119d139
Matt H <matt@designengine.ai>
2018-10-31 16:26:16 -0700

    Home page refactoring

--- src/components/iterables/ArtboardItem.jsx
+++ src/components/iterables/ArtboardItem.jsx
@@ -19,18 +19,18 @@ class ArtboardItem extends Component {
 	};
 
 	render() {
-		const imageClass = (this.props.size === 'landscape') ? 'part-item-image part-item-image-landscape' : 'part-item-image part-item-image-portrait';
-		const titleClass = (this.state.isSelected) ? 'part-item-title is-hidden' : 'part-item-title';
-		const btnClass = (this.state.isSelected) ? 'part-item-button part-item-button-selected' : 'part-item-button';
+		const imageClass = (this.props.size === 'landscape') ? 'artboard-item-image artboard-item-image-landscape' : 'artboard-item-image artboard-item-image-portrait';
+		const titleClass = (this.state.isSelected) ? 'artboard-item-title is-hidden' : 'artboard-item-title';
+		const btnClass = (this.state.isSelected) ? 'artboard-item-button artboard-item-button-selected' : 'artboard-item-button';
 
 		return (
-			<div className="part-item">
+			<div className="artboard-item">
 				<img className={imageClass} src={this.props.image} alt={this.props.title} onClick={()=> this.props.onClick()} />
-				<div className="part-item-details-wrapper">
+				<div className="artboard-item-details-wrapper">
 					<div className={titleClass}>{this.props.title}</div>
 					<button className={btnClass} onClick={()=> this.handleSelect()}>
 						{(this.state.isSelected)
-							? <FontAwesome name="check" className="part-item-check" />
+							? <FontAwesome name="check" className="artboard-item-check" />
 							: 'Select'
 						}
 					</button>
