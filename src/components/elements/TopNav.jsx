c70abead7602c632d8de269fe5ca3767c6e05cb2
Matt H <matt@designengine.ai>
2018-11-09 11:38:41 -0800

    Removing dropdowns from top

--- src/components/elements/TopNav.jsx
+++ src/components/elements/TopNav.jsx
@@ -2,9 +2,7 @@
 import React, { Component } from 'react';
 import './TopNav.css';
 
-import axios from 'axios';
 import cookie from 'react-cookies';
-import FontAwesome from 'react-fontawesome';
 import { Column, Row } from 'simple-flexbox';
 
 class TopNav extends Component {
@@ -19,102 +17,8 @@ class TopNav extends Component {
 	}
 
 	componentDidMount() {
-		//this.refreshData();
 	}
 
-	refreshData = ()=> {
-		let formData = new FormData();
-		formData.append('action', 'UPLOAD_DOCS');
-		formData.append('user_id', cookie.load('user_id'));
-		axios.post('https://api.designengine.ai/system.php', formData)
-			.then((response) => {
-				console.log('UPLOAD_DOCS', response.data);
-				let uploads = [];
-				response.data.uploads.forEach((upload) => {
-					uploads.push({
-						id       : upload.id,
-						title    : upload.title,
-						added    : upload.added,
-						selected : (upload.id === cookie.load('upload_id')),
-						key      : 'uploads'
-					});
-				});
-
-				uploads.push({
-					id       : -1,
-					title    : 'Upload',
-					added    : '0000-00-00 00:00:00',
-					selected : false,
-					key      : 'uploads'
-				});
-
-				this.setState({ uploads : uploads });
-
-				formData.append('action', 'COLORS');
-				formData.append('artboard_id', this.props.artboardID);
-				axios.post('https://api.designengine.ai/system.php', formData)
-					.then((response) => {
-						console.log('COLORS', response.data);
-						let colors = [];
-						response.data.colors.forEach((color) => {
-							colors.push({
-								id       : color.id,
-								title    : color.title,
-								selected : false,
-								key      : 'colors'
-							});
-						});
-						this.setState({ colors : colors });
-
-						formData.append('action', 'DEVICES');
-						formData.append('upload_id', cookie.load('upload_id'));
-						axios.post('https://api.designengine.ai/system.php', formData)
-							.then((response) => {
-								console.log('DEVICES', response.data);
-								let devices = [];
-								response.data.devices.forEach((device) => {
-									devices.push({
-										id       : device.id,
-										title    : device.title,
-										selected : false,
-										key      : 'devices'
-									});
-								});
-								this.setState({ devices : devices });
-							}).catch((error) => {
-						});
-					}).catch((error) => {
-				});
-			}).catch((error) => {
-		});
-	};
-
-	toggleSelected = (id, ind, key) => {
-		console.log('toggleSelected()', 'id='+id, 'ind='+ind, 'key='+key);
-		let tmp = [...this.state[key]];
-		tmp[ind].selected = !tmp[ind].selected;
-		this.setState({ [key] : tmp });
-// 		this.handleDropdownUpdate(key, tmp[id-1].title);
-	};
-
-	resetThenSet = (ind, key) => {
-		console.log('resetThenSet()', ind, key);
-		let tmp = [...this.state[key]];
-		tmp.forEach(item => item.selected = false);
-		tmp[ind].selected = true;
-		this.ddTitle = tmp[ind].title;
-		this.setState({ [key] : tmp });
-
-		if (key === 'uploads') {
-			if (tmp[ind].id !== -1) {
-				cookie.save('upload_id', tmp[ind].id);
-				window.location = '/';
-
-			} else {
-				this.props.onUpload();
-			}
-		}
-	};
 
 	render() {
 		return (
@@ -131,7 +35,7 @@ class TopNav extends Component {
 				<div className="top-nav-column top-nav-column-right">
 					{(cookie.load('user_id') !== '0') && (
 						<Column flexGrow={1} horizontal="end" vertical="center">
-							<button className="top-nav-upload-button" onClick={()=> this.props.onUpload()}><FontAwesome name="plus" className="top-nav-upload-plus" /> New</button>
+							<button className="top-nav-upload-button" onClick={()=> this.props.onUpload()}>New Project</button>
 						</Column>
 					)}
 				</div>
