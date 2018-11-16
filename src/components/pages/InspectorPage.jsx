8c59e9c2ca725af0e30a78750e85a00a2215d274
Matt H <matt@designengine.ai>
2018-11-16 13:10:35 -0800

    resolving issue #98

--- src/components/pages/InspectorPage.jsx
+++ src/components/pages/InspectorPage.jsx
@@ -367,8 +367,8 @@ class InspectorPage extends Component {
 
 // 		const panelImageClass = 'inspector-page-panel-image' + ((artboard && !slice) ? ' inspector-page-panel-image-artboard' : '');// + ((slice) ? ((slice.meta.frame.size.width > slice.meta.frame.size.height) ? ' inspector-page-panel-image-landscape' : ' inspector-page-panel-image-portrait')  + ' ' + ((slice.type === 'slice') ? 'inspector-page-panel-image-slice' : (slice.type === 'hotspot') ? 'inspector-page-panel-image-hotspot' : (slice.type === 'textfield') ? 'inspector-page-panel-image-textfield' : 'inspector-page-panel-image-background') : '');
 		const panelImageClass = 'inspector-page-panel-image' + ((artboard && !slice) ? ' inspector-page-panel-image-artboard' + ((panelFrame.size.width > panelFrame.size.height) ? ' inspector-page-panel-image-landscape' : ' inspector-page-panel-image-portrait') : '');
-		const panelSliceImage = (slice) ? ((slice.type === 'slice') ? slice.filename + '@' + scaleSize + 'x.png' : ('https://via.placeholder.com/' + (slice.meta.frame.size.width * scaleSize) + 'x' + (slice.meta.frame.size.height * scaleSize))) : null;
-// 		const panelSliceImage = (slice) ? slice.filename + '@' + scaleSize + 'x.png' : null;
+// 		const panelSliceImage = (slice) ? ((slice.type === 'slice') ? slice.filename + '@' + scaleSize + 'x.png' : ('https://via.placeholder.com/' + (slice.meta.frame.size.width * scaleSize) + 'x' + (slice.meta.frame.size.height * scaleSize))) : null;
+		const panelSliceImage = (slice) ? slice.filename + '@' + scaleSize + 'x.png' : null;
 
 		const styles = (slice &&  slice.meta.styles && slice.meta.styles.length > 0) ? {
 			stroke : (slice.meta.styles[0].border.length > 0) ? {
