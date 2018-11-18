f008466525a4ca67dc7afba48c9d1ae5625b7ec1
Matt H <matt@designengine.ai>
2018-11-18 10:21:30 -0800

    resolving issue #141

--- src/components/elements/TopNav.jsx
+++ src/components/elements/TopNav.jsx
@@ -30,7 +30,10 @@ class TopNav extends Component {
 				<div className="top-nav-column top-nav-column-left"><Row horizontal="start" vertical="center">
 					<img onClick={()=> this.props.onHome()} src="/images/logo.svg" className="top-nav-logo" alt="Design Engine" />
 					<div className={(window.location.pathname === '/') ? 'top-nav-link top-nav-link-selected' : 'top-nav-link'} onClick={()=> this.props.onHome()}>Projects</div>
-					<div className={(window.location.pathname.includes('/add-ons')) ? 'top-nav-link top-nav-link-selected' : 'top-nav-link'} onClick={()=> this.props.onPage('add-ons')}>Add Ons</div>
+					<Row>
+						<Column><div className={(window.location.pathname.includes('/add-ons')) ? 'top-nav-link top-nav-link-selected' : 'top-nav-link'} onClick={()=> this.props.onPage('add-ons')}>Add Ons</div></Column>
+						<Column><sup>new</sup></Column>
+					</Row>
 					<div className="top-nav-link" onClick={()=> window.open('https://github.com/de-ai/designengine.ai/projects/1')}>Roadmap</div>
 				</Row></div>
 
