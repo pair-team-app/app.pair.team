dc509bb836663856a4a725d25501b6139b1ef21b
Matt H <matt@designengine.ai>
2018-12-04 20:38:39 -0800

    recover page

--- src/components/pages/RecoverPage.jsx
+++ src/components/pages/RecoverPage.jsx
@@ -56,7 +56,7 @@ class RecoverPage extends Component {
 
 		return (
 			<div className="page-wrapper recover-page-wrapper">
-				<h4>Reset Password</h4>
+				<h4>Forgot Password</h4>
 				Enter the email address of each member of your team to invite them to this project.
 				<div className="recover-page-form-wrapper">
 					{(errorMsg !== '') && (<div className="input-wrapper input-wrapper-error"><input type="text" placeholder="" value={errorMsg} disabled /></div>)}
