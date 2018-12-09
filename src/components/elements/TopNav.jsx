5254c222aeebf9b7b60a57cad2e04f04df7153c2
Matt H <matt@designengine.ai>
2018-12-08 17:23:28 -0800

    resolving issue #245

--- src/components/elements/TopNav.jsx
+++ src/components/elements/TopNav.jsx
@@ -30,7 +30,7 @@ class TopNav extends Component {
 			<div className="top-nav-wrapper">
 				<div className="top-nav-column top-nav-column-left"><Row horizontal="start" vertical="center">
 					<img onClick={()=> this.props.onHome()} src="/images/logo.svg" className="top-nav-logo" alt="Design Engine" />
-					<div className={(window.location.pathname === '/' || window.location.pathname.includes('proj')) ? 'top-nav-link top-nav-link-selected' : 'top-nav-link'} onClick={()=> this.props.onHome()}>Projects</div>
+					<div className={(window.location.pathname === '/' || window.location.pathname.includes('proj') || window.location.pathname.includes('explore')) ? 'top-nav-link top-nav-link-selected' : 'top-nav-link'} onClick={()=> this.props.onHome()}>Projects</div>
 					{/*<div className={(window.location.pathname.includes('/add-ons')) ? 'top-nav-link top-nav-link-selected' : 'top-nav-link'} onClick={()=> this.props.onPage('add-ons')}>Add Ons</div>*/}
 					<div className={(window.location.pathname.includes('/explore')) ? 'top-nav-link top-nav-link-selected' : 'top-nav-link'} onClick={()=> this.props.onPage('explore')}>Explore</div>
 					<div className="top-nav-link" onClick={()=> window.open('https://docs.google.com/forms/d/e/1FAIpQLSdYZI6uIqF9D5zW5LmZQqCem6zrXh7THmVVBoOkeAQWm9o6lg/viewform?usp=sf_link')}>Survey</div>
