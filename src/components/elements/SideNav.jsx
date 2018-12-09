e82b14f881a0fae45a6b7a85bb255285eb66499c
Matt H <matt@designengine.ai>
2018-12-08 17:24:51 -0800

    resolving issue #238

--- src/components/elements/SideNav.jsx
+++ src/components/elements/SideNav.jsx
@@ -328,7 +328,7 @@ class SideNav extends Component {
 						{(cookie.load('user_id') === '0') ? (<div>
 							<span className="side-nav-subtext">You must be logged in.</span>
 						</div>) : (<div>
-								{(uploads.length === 0) ? <span className="side-nav-subtext">You don't have any projects yet!</span> : uploads.map((upload, i) => {
+								{(uploads.length === 0) ? <span className="side-nav-subtext">Loading projects…</span> : uploads.map((upload, i) => {
 								return (
 									<UploadTreeItem
 										key={i}
