6b1a76f510e4b747ea1c1600df4b932bcf574543
mtholcombe <matt@designengine.ai>
2018-12-11 20:49:59 -0800

    resolving issue #258

--- src/components/elements/BottomNav.jsx
+++ src/components/elements/BottomNav.jsx
@@ -25,7 +25,7 @@ class BottomNav extends Component {
 					<div className="bottom-nav-link" onClick={()=> this.props.onPage('privacy')}>Privacy</div>
 					<div className="bottom-nav-link" onClick={()=> window.open('https://join.slack.com/t/designengineai/shared_invite/enQtMzE5ODE0MTA0MzA5LWM2NzcwNTRiNjQzMTAyYTEyNjQ1MjE5NmExNDM1MzAyNWZjMTA0ZWIwNTdmZjYyMjc2M2ExNjAyYWFhZDliMzA')}>Slack</div>
 					<div className="bottom-nav-link" onClick={()=> window.open('https://spectrum.chat/designengine')}>Spectrum</div>
-					<div className="bottom-nav-link" onClick={()=> this.props.onPage('api')}>API</div>
+					{/*<div className="bottom-nav-link" onClick={()=> this.props.onPage('api')}>API</div>*/}
 					<div className="bottom-nav-link" onClick={()=> window.open('https://github.com/de-ai/designengine.ai/projects/1')}>Roadmap</div>
 					{(cookie.load('user_id') === '0') && (<div className="bottom-nav-link" onClick={() => this.props.onPage('register')}>Sign Up</div>)}
 					{(cookie.load('user_id') === '0') && (<div className="bottom-nav-link" onClick={() => this.props.onPage('login')}>Login</div>)}
