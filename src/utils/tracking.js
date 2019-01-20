518ed7fb73968a7567a2d1550342cd425ac8dfe6
mtholcombe <matt@builtinmenlo.com>
2019-01-20 15:25:24 -0800

    adding modal tracking

--- src/utils/tracking.js
+++ src/utils/tracking.js
@@ -2,16 +2,16 @@
 import cookie from "react-cookies";
 import ReactGA from 'react-ga';
 
+
+const UA_ID = 'UA-74998463-1';
+
+
 export function initTracker(userID) {
-ReactGA.initialize('UA-74998463-1', {
+ReactGA.initialize(UA_ID, {
 // 		debug     : true,
 		titleCase : false,
 		gaOptions : { userId : userID }
 	});
-}
-
-export function trackPageview() {
-	ReactGA.pageview(window.location.pathname + window.location.search);
 }
 
 export function trackEvent(category, action, label=null, value=null, nonInteraction=false) {
@@ -19,4 +19,12 @@ export function trackEvent(category, action, label=null, value=null, nonInteract
 	value = parseInt(value || ((typeof cookie.load('user_id') !== 'undefined') ? cookie.load('user_id') : '0'), 10);
 
 	ReactGA.event({ category, action, label, value, nonInteraction });
+}
+
+export function trackModal(type) {
+	ReactGA.pageview(`${window.location.pathname}/${type}`);
+}
+
+export function trackPageview() {
+	ReactGA.pageview(`${window.location.pathname}${window.location.search}`);
 }
