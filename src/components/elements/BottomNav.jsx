a6744eaafbef4c9a8a330d99b15d7f672401e68c
mtholcombe <matt@builtinmenlo.com>
2019-01-22 15:36:47 -0800

    converting stateless components to functions

--- src/components/elements/BottomNav.jsx
+++ src/components/elements/BottomNav.jsx
@@ -1,42 +1,33 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import './BottomNav.css';
 
 import { isUserLoggedIn } from '../../utils/funcs';
 
 
-class BottomNav extends Component {
-	constructor(props) {
-		super(props);
+function BottomNav(props) {
+	console.log('BottomNav()', props);
 
-		this.state = {
-		};
-	}
+	return (
+		<div className="bottom-nav-wrapper">
+			<div className="bottom-nav-link-wrapper">
+				<div className="bottom-nav-link" onClick={()=> props.onPage('inspect')}>Free Inspect</div>
+				<div className="bottom-nav-link" onClick={()=> props.onPage('parts')}>Free Parts</div>
+				<div className="bottom-nav-link" onClick={()=> props.onPage('terms')}>Terms</div>
+				<div className="bottom-nav-link" onClick={()=> props.onPage('privacy')}>Privacy</div>
 
-	render() {
-// 		console.log('BottomNav.render()', this.props, this.state);
-
-		return (
-			<div className="bottom-nav-wrapper">
-				<div className="bottom-nav-link-wrapper">
-					<div className="bottom-nav-link" onClick={()=> this.props.onPage('inspect')}>Free Inspect</div>
-					<div className="bottom-nav-link" onClick={()=> this.props.onPage('parts')}>Free Parts</div>
-					<div className="bottom-nav-link" onClick={()=> this.props.onPage('terms')}>Terms</div>
-					<div className="bottom-nav-link" onClick={()=> this.props.onPage('privacy')}>Privacy</div>
-
-					{(isUserLoggedIn())
-						? (<div className="bottom-nav-link" onClick={() => this.props.onLogout()}>Sign Out</div>)
-						: (<span style={{ display : 'inline' }}>
-								<div className="bottom-nav-link" onClick={()=> this.props.onPage('register')}>Sign Up</div>
-								<div className="bottom-nav-link" onClick={()=> this.props.onPage('login')}>Login</div>
-						</span>)
-					}
-				</div>
-
-				<div className="copyright">&copy; {new Date().getFullYear()} Design Engine AI, Inc</div>
+				{(isUserLoggedIn())
+					? (<div className="bottom-nav-link" onClick={() => props.onLogout()}>Sign Out</div>)
+					: (<span style={{ display : 'inline' }}>
+							<div className="bottom-nav-link" onClick={()=> props.onPage('register')}>Sign Up</div>
+							<div className="bottom-nav-link" onClick={()=> props.onPage('login')}>Login</div>
+					</span>)
+				}
 			</div>
-		);
-	}
+
+			<div className="copyright">&copy; {new Date().getFullYear()} Design Engine AI, Inc</div>
+		</div>
+	);
 }
 
 export default BottomNav;
--- src/components/forms/elements/RadioButton.jsx
+++ src/components/forms/elements/RadioButton.jsx
@@ -1,5 +1,5 @@
 
-import React, { Component } from 'react';
+import React from 'react';
 import './RadioButton.css';
 
 import { Column, Row } from 'simple-flexbox';
@@ -7,33 +7,21 @@ import { Column, Row } from 'simple-flexbox';
 import radButtonSelected from '../../../assets/images/buttons/btn-radio-button_selected.png'
 
 
-class RadioButton extends Component {
-	constructor(props) {
-		super(props);
+function RadioButton(props) {
+	const { enabled, selected, title, subtext } = props;
 
-		this.state = {
-		};
-	}
+	const titleClass = (enabled) ? 'radio-button-title' : 'radio-button-title radio-button-title-disabled';
+	const subtextClass = (enabled) ? 'radio-button-subtext' : 'radio-button-subtext radio-button-subtext-disabled';
 
-	componentDidMount() {
-	}
-
-	render() {
-		const { enabled, selected, title, subtext } = this.props;
-
-		const titleClass = (enabled) ? 'radio-button-title' : 'radio-button-title radio-button-title-disabled';
-		const subtextClass = (enabled) ? 'radio-button-subtext' : 'radio-button-subtext radio-button-subtext-disabled';
-
-		return (
-			<div className="radio-button"><Row vertical="center">
-				<Column vertical="center"><button disabled={(!enabled)} className="radio-button-button" onClick={()=> this.props.onClick()}>
-					{(selected) && (<img className="radio-button-image" src={radButtonSelected} alt={title} />)}
-				</button></Column>
-				<Column vertical="center"><div className={titleClass}>{title}</div></Column></Row>
-				<div className={subtextClass}>{subtext}</div>
-			</div>
-		);
-	}
+	return (
+		<div className="radio-button"><Row vertical="center">
+			<Column vertical="center"><button disabled={(!enabled)} className="radio-button-button" onClick={()=> this.props.onClick()}>
+				{(selected) && (<img className="radio-button-image" src={radButtonSelected} alt={title} />)}
+			</button></Column>
+			<Column vertical="center"><div className={titleClass}>{title}</div></Column></Row>
+			<div className={subtextClass}>{subtext}</div>
+		</div>
+	);
 }
 
 export default RadioButton;
