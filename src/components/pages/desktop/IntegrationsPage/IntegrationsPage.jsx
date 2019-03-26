af7eb420b070aeb62806327dca077ea514f4c63d
mtholcombe <matt@builtinmenlo.com>
2019-03-26 13:51:37 -0700

    adding grid to integrations page

--- src/components/pages/desktop/IntegrationsPage/IntegrationsPage.jsx
+++ src/components/pages/desktop/IntegrationsPage/IntegrationsPage.jsx
@@ -2,7 +2,45 @@
 import React, { Component } from 'react';
 import './IntegrationsPage.css';
 
+import { Column, Row } from 'simple-flexbox';
+
 import BaseDesktopPage from '../BaseDesktopPage';
+import { trackEvent } from '../../../../utils/tracking';
+import integrationItems from '../../../../assets/json/integration-items';
+
+
+const IntegrationsPageGrid = (props)=> {
+	console.log('IntegrationsPage.IntegrationsPageGrid()', props);
+
+	const { integrations } = props;
+	return (<div className="integrations-page-grid">
+		<Row horizontal="start" className="integrations-page-grid-item-wrapper" style={{ flexWrap : 'wrap' }}>
+			{integrations.map((integration, i) => {
+				return (<Column key={i}>
+					<IntegrationsPageGridItem
+						title={integration.title}
+						image={integration.filename}
+						onClick={()=> props.onClick(integration)} />
+				</Column>);
+			})}
+		</Row>
+	</div>);
+};
+
+
+const IntegrationsPageGridItem = (props)=> {
+	console.log('IntegrationsPage.IntegrationsPageGridItem()', props);
+
+	const { title, image } = props;
+	return (<div className="integrations-page-grid-item" onClick={()=> props.onClick()}>
+		<img className="integrations-page-grid-item-image" src={image} alt={title} />
+		<div className="integrations-page-grid-item-overlay" />
+		<div className="integrations-page-grid-item-title-wrapper">
+			<div className="home-page-artboard-grid-item-title">{title}</div>
+		</div>
+	</div>);
+};
+
 
 class IntegrationsPage extends Component {
 	constructor(props) {
@@ -12,13 +50,21 @@ class IntegrationsPage extends Component {
 		};
 	}
 
+	handleIntegrationItemClick = (integration)=> {
+		console.log('IntegrationsPage.handleIntegrationItemClick()', integration);
+		trackEvent('integration', 'click', integration.title);
+	};
+
 	render() {
+		console.log('IntegrationsPage.render()', this.props, this.state);
+
 		return (
 			<BaseDesktopPage className="integrations-page-wrapper">
 				<h4>CD / CI Integrations</h4>
-				<div className="integrations-page-grid-wrapper">
-
-				</div>
+				<IntegrationsPageGrid
+					integrations={integrationItems}
+					onClick={this.handleIntegrationItemClick}
+				/>
 			</BaseDesktopPage>
 		);
 	}
