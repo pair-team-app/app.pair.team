84b7c5c7254d9b16b268d0d3c3d5c91754666a49
mtholcombe <matt@designengine.ai>
2019-10-10 17:45:25 -0700

    add footer buttons

--- src/components/sections/BottomNav/BottomNav.jsx
+++ src/components/sections/BottomNav/BottomNav.jsx
@@ -5,10 +5,22 @@ import './BottomNav.css';
 import PageNavLink from '../../iterables/PageNavLink';
 import navLinks from '../../../assets/json/nav-links';
 
+import { GITHUB_XD_PLUGIN, NPM_DE_PLAYGROUND } from '../../../consts/uris';
+
 
 function BottomNav(props) {
+	const handleClick = (event, url)=> {
+		event.preventDefault();
+		window.open(url);
+	};
+
 	return (
 		<div className="bottom-nav">
+			<div className="bottom-nav-spacer" />
+			<div className="bottom-nav-button-wrapper">
+				<button className="quiet-button adjacent-button" onClick={(event)=> handleClick(event, NPM_DE_PLAYGROUND)}>Download NPM</button>
+				<button className="quiet-button" onClick={(event)=> handleClick(event, GITHUB_XD_PLUGIN)}>Open in Adobe XD</button>
+			</div>
 			<div className="bottom-nav-link-wrapper">
 				{(navLinks.bottom.map((navLink, i)=> (
 					<PageNavLink key={i} navLink={navLink} onClick={(event)=> null} />
--- src/consts/uris.js
+++ src/consts/uris.js
@@ -25,12 +25,11 @@ export const API_ENDPT_URL = `${API_HOSTNAME}/playgrounds.php`;
 // export const API_ENDPT_URL = `${API_HOSTNAME}/dev.php`;
 
 export const GITHUB_APP_AUTH = 'https://github.com/login/oauth/authorize?client_id=e6f08e86acdc4e4cca38&redirect_uri=https://api.designengine.ai/system.php&scope=gist,user:email&state=__{EPOCH}__';
-export const GITHUB_ORG = 'https://www.github.com/de-ai';
 export const GITHUB_CHANGELOG = 'https://github.com/de-ai/designengine.ai/blob/master/changelog.md';
 export const GITHUB_ROADMAP = 'https://github.com/orgs/de-ai/projects/7';
-export const GITHUB_REPO_XD = 'https://github.com/de-ai/designengine-xd';
+export const GITHUB_XD_PLUGIN = 'https://github.com/de-ai/designengine-xd';
 
-export const NPM_PACKAGE = 'https://www.npmjs.com/package/design-engine-playground';
+export const NPM_DE_PLAYGROUND = 'https://www.npmjs.com/package/design-engine-playground';
 
 export const SLACK_INVITE = 'https://join.slack.com/t/designengineai/shared_invite/enQtMzE5ODE0MTA0MzA5LWM2NzcwNTRiNjQzMTAyYTEyNjQ1MjE5NmExNDM1MzAyNWZjMTA0ZWIwNTdmZjYyMjc2M2ExNjAyYWFhZDliMzA';
 export const SOCIAL_TWITTER = 'https://twitter.com/designengineai';
