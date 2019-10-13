e465c196bf16dd4e31abd5121c111aad73315552
mtholcombe <matt@designengine.ai>
2019-10-13 09:04:17 -0700

    expander animation

--- src/components/iterables/SectionExpander/SectionExpander.jsx
+++ src/components/iterables/SectionExpander/SectionExpander.jsx
@@ -16,7 +16,7 @@ function SectionExpander(props) {
 			isOpen={open}
 			className={`section-expander-content-wrapper${(open) ? ' section-expander-content-wrapper-open' : ''}`}
 // 				transition={`height ${(open) ? '250ms cubic-bezier(0.2, 0.5, 0.9, 1.0)' : '250ms cubic-bezier(0.5, 0.9, 0.1, 1.0)'}`}
-			transition={`height ${(open) ? '333ms cubic-bezier(0.5, 0.9, 0.1, 1.0)' : '250ms cubic-bezier(0.0, 1.0, 0.0, 1.0)'}`}
+			transition={`height ${(open) ? '333ms cubic-bezier(0.2, 0.9, 0.1, 1.0)' : '333ms cubic-bezier(0.0, 1.0, 0.0, 1.0)'}`}
 			aria-hidden={!open}
 			render={(state)=> (<div className="section-expander-content">{children}</div>)}>
 		</Collapse>
