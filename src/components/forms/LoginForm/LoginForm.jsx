5b368a65d52b72357ec059b672a8b924acda9e25
mtholcombe <matt@designengine.ai>
2019-11-26 17:53:13 -0800

    fixing stuck submit button after error

--- src/components/forms/LoginForm/LoginForm.jsx
+++ src/components/forms/LoginForm/LoginForm.jsx
@@ -121,11 +121,11 @@ class LoginForm extends Component {
 				}
 
 				{(passMsg)
-					? (<input type="email" placeholder="Enter Password" value={(passMsg || password)} onFocus={()=> this.setState({ passMsg : null })} onChange={(event)=> this.setState({ password : event.target.value, passMsg : null })} onClick={this.handlePassword} ref={(element)=> { this.passwordTextfield = element }} autoComplete="off" required />)
+					? (<input type="email" placeholder="Enter Password" value={(passMsg || password)} onFocus={()=> this.setState({ passwordValid : true, passMsg : null })} onChange={(event)=> this.setState({ password : event.target.value, passMsg : null })} onClick={this.handlePassword} ref={(element)=> { this.passwordTextfield = element }} autoComplete="off" required />)
 					: (<input type="password" placeholder="Enter Password" value={(passMsg || password)} onChange={(event)=> this.setState({ password : event.target.value, passMsg : null })} onClick={this.handlePassword} ref={(element)=> { this.passwordTextfield = element }} autoComplete="off" />)
 				}
 
-				<button disabled={(email.length === 0 || !emailValid || !passwordValid)} type="submit" onClick={(event)=> this.handleSubmit(event)}>Login</button>
+				<button disabled={(email.length === 0 || password.length === 0 || !emailValid || !passwordValid || passMsg)} type="submit" onClick={(event)=> this.handleSubmit(event)}>Login</button>
 			</form>
 		</div>);
 	}
