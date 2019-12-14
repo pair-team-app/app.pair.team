8771505ed9a2ca7f38e0995041d97eada77a4838
mtholcombe <matt@designengine.ai>
2019-12-14 10:47:15 -0800

    left off

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,7 @@
 
 import cookie from 'react-cookies';
 import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 import { fetchComponentTypes, fetchEventGroups, fetchProducts, fetchUserProfile } from '../actions';
@@ -8,7 +9,36 @@ import rootReducer from '../reducers/index';
 import { onMiddleware } from '../middleware'
 
 
-const store = createStore(rootReducer, applyMiddleware(onMiddleware, thunk));
+//const store = createStore(rootReducer, applyMiddleware(onMiddleware, thunk));
+
+const composeEnhancers = composeWithDevTools({
+	// options like actionSanitizer, stateSanitizer
+});
+const store = createStore(rootReducer, composeEnhancers(
+	applyMiddleware(onMiddleware, thunk),
+	// other store enhancers if any
+));
+
+
+
+//const composeEnhancers =
+//	window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+//		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+//			// options like actionSanitizer, stateSanitizer
+//		}) : compose;
+
+
+
+
+//const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+//const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(applyMiddleware(onMiddleware, thunk)
+//));
+
+//const store = createStore(
+//	rootReducer,
+//	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+//	applyMiddleware(onMiddleware, thunk)
+//);
 
 
 if (typeof cookie.load('user_id') === 'undefined') {
