6b123590dd01cf20cfab990b47bc3bc365206d38
mtholcombe <matt@pairurl.com>
2020-08-24 16:24:06 -0700

    cleanup

--- src/utils/reform.js
+++ src/utils/reform.js
@@ -35,46 +35,26 @@ export const reformComment = (comment, uri, overwrite={})=> {
 export const reformComponent = (component, uri, componentTypes=null, overwrite={})=> {
   // console.log('reformComponent()', { keys : Object.keys(component), component, uri, componentTypes, overwrite });
 
-  // const { id, type_id, event_type_id, node_id, title, tag_name, sizes, image_url, html, styles, accessibility, meta, comments } = component;
   const { id, type_id, node_id, title, tag_name, sizes, image_url, comments } = component;
-  // const { width, height } = meta.bounds;
-  // const scale = 2;//Math.max(1, sizes['f'].width / meta.bounds.width, sizes['f'].height / meta.bounds.height);
 
   delete component['type_id'];
-  // delete component['event_type_id'];
   delete component['node_id'];
   delete component['tag_name'];
   delete component['image_url'];
-  // delete component['root_styles'];
-  // delete component['image_data'];
-  // delete component['thumb_data'];
 
   const reformed = {...component,
-    id            : id << 0,
-    typeID        : type_id << 0,
-    // eventTypeID   : event_type_id << 0,
-    nodeID        : node_id << 0,
-    title         : title.length === 0 ? tag_name : title,
-    tagName       : tag_name,
-    // styles        : (styles) ? JSON.parse(styles) : null,
-    // accessibility : (accessibility) ? JSON.parse(accessibility) : null,
-    typeGroup     : componentTypes.find(({ id })=> (id === type_id)),
-    // sizes         : { ...sizes, f : { width : width * (1 / scale), height : height * (1 / scale) }, o : { width : width * scale, height : height * scale } },
-    sizes         : { ...sizes },
-    images        : Object.keys(sizes).map((key)=> (`${image_url}_${key}.png`)),
-    comments      : (comments) ? comments.map((comment)=> reformComment(comment, `${uri}/${id}/comments`)).sort((i, ii)=> ((i.epoch > ii.epoch) ? -1 : (i.epoch < ii.epoch) ? 1 : ((i.type === 'bot') ? -1 : (ii.type === 'bot') ? 1 : 0))) : [],
-    // uri           : `${uri}/${id}`,
-    selected      : false,
-    // processed     : ((html && accessibility) !== null),
+    id        : id << 0,
+    typeID    : type_id << 0,
+    nodeID    : node_id << 0,
+    title     : (title.length === 0) ? tag_name : title,
+    tagName   : tag_name,
+    typeGroup : componentTypes.find(({ id })=> (id === type_id)),
+    sizes     : { ...sizes },
+    images    : Object.keys(sizes).map((key)=> (`${image_url}_${key}.png`)),
+    comments  : (comments) ? comments.map((comment)=> reformComment(comment, `${uri}/${id}/comments`)).sort((i, ii)=> ((i.epoch > ii.epoch) ? -1 : (i.epoch < ii.epoch) ? 1 : ((i.type === 'bot') ? -1 : (ii.type === 'bot') ? 1 : 0))) : [],
+    selected  : false,
     ...overwrite
   };
-
-  // if ((html && accessibility) !== null) {
-  //   console.log('[%s] .::(REFORMED)::.', component.id, { data : { ...reformed, size : jsonFormatKB(reformed) } });
-
-  // } else {
-  //   console.log('[%s] .::(INITIAL)::.', component.id, { ...reformed, size : jsonFormatKB(reformed) });
-  // }
 
   // console.log('reformComponent()', { component, uri, componentTypes, overwrite, reformed });
   return { ...reformed, size: jsonFormatKB(reformed) };
@@ -103,11 +83,11 @@ export const reformPlayground = (playground, devices=null, componentTypes=null, 
 
   const { build_id, team_id, device_id, title, type_groups, components, added, last_visited } = playground;
 
-  // delete playground['build_id'];
-  // delete playground['team_id'];
-  // delete playground['device_id'];
-  // delete playground['type_groups'];
-  // delete playground['last_visited'];
+  delete playground['build_id'];
+  delete playground['team_id'];
+  delete playground['device_id'];
+  delete playground['type_groups'];
+  delete playground['last_visited'];
 
   const slug = Strings.slugifyURI(title);
   const device = (devices.find(({ id })=> (id === (device_id << 0))) || null);
@@ -118,15 +98,12 @@ export const reformPlayground = (playground, devices=null, componentTypes=null, 
     buildID     : build_id << 0,
     deviceID    : device_id << 0,
     team        : (!playground.team || playground.team.length === 0) ? team : playground.team,
-    // components  : (components || []),
     components  : (components && components.length > 0) ? components.map((component)=> (reformComponent(component, uri, componentTypes))) : [],
     typeGroups  : type_groups.map((typeGroupID)=> (componentTypes.find(({ id })=> ((typeGroupID << 0) === id)))),
     device      : (device || device_id),
     selected    : false,
     lastVisited : moment(last_visited).utc(),
-    added       : (playground.added)
-      ? moment(added).add(moment().utcOffset() << 0, 'minute')
-      : moment.utc(),
+    added       : (playground.added) ? moment(added).add(moment().utcOffset() << 0, 'minute') : moment.utc(),
     ...overwrite
   };
 
