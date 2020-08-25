b0b59992e3a59b53dc6b2e99c5b483d9ab91e4ce
mtholcombe <matt@pairurl.com>
2020-08-25 15:03:50 -0700

    adding context menu

--- src/components/sections/ProjectOverlay/ProjectOverlay.jsx
+++ src/components/sections/ProjectOverlay/ProjectOverlay.jsx
@@ -2,11 +2,11 @@
 import React, { Component } from 'react';
 import './ProjectOverlay.css';
 
+import KeyboardEventHandler from 'react-keyboard-event-handler';
+import { Menu, Item, MenuProvider } from 'react-contexify';
 import { connect } from 'react-redux';
-// import { withRouter } from 'react-router-dom';
 
 import { setComment, setComponent, setPlayground } from '../../../redux/actions';
-
 
 
 class ProjectOverlay extends Component {
@@ -25,9 +25,20 @@ class ProjectOverlay extends Component {
     console.log('%s.componentDidUpdate()', this.constructor.name, { prevProps, props : this.props, prevState, state : this.state });
   }
 
+  handleAddComment = (event)=> {
+    console.log('handleAddComment()', { event });
+  }
+
   handleCommentMarkerClick = (comment)=> {
     console.log('handleCommentMarkerClick()', { comment });
     this.props.setComment(comment);
+  };
+
+  handleKeyPress = (event, key)=> {
+    console.log('handleKeyPress()', { event, key });
+
+    this.props.setComment(null);
+    this.props.setComponent(null);
   };
 
   render() {
@@ -36,12 +47,16 @@ class ProjectOverlay extends Component {
     const { playground, component } = this.props;
     return (<div className="project-overlay">
       <div className="header-wrapper">HEADER</div>
-      <div className="content-wrapper">
-        <img src={component.images.pop()} alt={playground.title} />
-        <div className="comments-wrapper">
-          {(component.comments.filter(({ types })=> (types.includes('op'))).map((comment, i)=> (<ProjectViewCommentMarker key={i} ind={(i+1)} comment={comment} onClick={this.handleCommentMarkerClick} />)))}
-        </div>
+      <div className="content-wrapper"><KeyboardEventHandler handleKeys={['esc']} onKeyEvent={(key, event)=> this.handleKeyPress(event, key)} />
+        <img src={component.images.pop()} alt={component.title} />
+        <MenuProvider id="menu_id" className="menu-provider">
+          <div className="comments-wrapper">
+            {(component.comments.filter(({ types })=> (types.includes('op'))).map((comment, i)=> (<ProjectViewCommentMarker key={i} ind={(i+1)} comment={comment} onClick={this.handleCommentMarkerClick} />)))}
+          </div>
+        </MenuProvider>
       </div>
+
+      <AddCommentMenu onClick={({ event, props })=> console.log('MenuClick', { event, props })} onSubmit={this.handleAddComment} />
     </div>);
   }
 }
@@ -62,6 +77,21 @@ const ProjectViewCommentMarker = (props)=> {
     <div>{ind}</div>
   </div>);
 };
+
+const AddCommentMenu = (props)=> {
+  console.log('AddCommentMenu()', { props });
+
+  return (<Menu id="menu_id" className="add-comment-menu">
+    <div className="form-wrapper">
+      <input type="text" className="comment-txt" placeholder="Add Comment" autoFocus />
+    </div>
+    <div className="button-wrapper button-wrapper-row">
+      <button onClick={props.onSubmit}>Submit</button>
+      <button className="cancel-button">Cancel</button>
+    </div>
+  </Menu>);
+};
+
 
 const mapDispatchToProps = (dispatch)=> {
   return ({
