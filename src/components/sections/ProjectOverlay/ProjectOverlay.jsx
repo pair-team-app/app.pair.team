d5924cf3cf09dfd0adba5b293d9c502cc41f353b
mtholcombe <matt@pairurl.com>
2020-08-25 16:43:22 -0700

    add comment from popover

--- src/components/sections/ProjectOverlay/ProjectOverlay.jsx
+++ src/components/sections/ProjectOverlay/ProjectOverlay.jsx
@@ -6,7 +6,7 @@ import KeyboardEventHandler from 'react-keyboard-event-handler';
 import { Menu, Item, MenuProvider } from 'react-contexify';
 import { connect } from 'react-redux';
 
-import { setComment, setComponent } from '../../../redux/actions';
+import { setComment, setComponent, makeComment } from '../../../redux/actions';
 
 
 class ProjectOverlay extends Component {
@@ -14,6 +14,9 @@ class ProjectOverlay extends Component {
     super(props);
 
     this.state = {
+      comment : {
+        text : ''
+      }
     };
   }
 
@@ -25,29 +28,59 @@ class ProjectOverlay extends Component {
     console.log('%s.componentDidUpdate()', this.constructor.name, { prevProps, props : this.props, prevState, state : this.state });
   }
 
-  handleAddComment = (event)=> {
+  handleAddComment = (event=null)=> {
     console.log('handleAddComment()', { event });
+
+    const { profile, component } = this.props;
+    const { comment } = this.state;
+
+    if (comment.text.length > 0) {
+      this.props.makeComment({
+        comment      : null,
+        position     : null,
+        content      : comment.text,
+        user_id      : profile.id,
+        team_id      : 0,
+        component_id : component.id,
+        format       : 'text'
+      });
+    }
+  }
+
+  handleAddCommentText = (text)=> {
+    console.log('handleAddCommentText()', { text });
+    const { comment } = this.state;
+
+    this.setState({ comment : { ...comment, text }})
   }
 
   handleCommentMarkerClick = (comment)=> {
     console.log('handleCommentMarkerClick()', { comment });
+
     this.props.setComment(comment);
   };
 
   handleKeyPress = (event, key)=> {
     console.log('handleKeyPress()', { event, key });
 
-    this.props.setComment(null);
-    this.props.setComponent(null);
+    if (key === 'esc') {
+      this.props.setComment(null);
+      this.props.setComponent(null);
+
+    } else {
+      this.handleAddComment(event);
+    }
   };
 
   render() {
     console.log('%s.render()', this.constructor.name, { props : this.props, state : this.state });
 
     const { component } = this.props;
+    const { comment } = this.state;
+
     return (<div className="project-overlay">
       <div className="header-wrapper">HEADER</div>
-      <div className="content-wrapper"><KeyboardEventHandler handleKeys={['esc']} onKeyEvent={(key, event)=> this.handleKeyPress(event, key)} />
+      <div className="content-wrapper"><KeyboardEventHandler handleKeys={['enter', 'esc']} handleFocusableElements onKeyEvent={(key, event)=> this.handleKeyPress(event, key)} />
         <img src={component.images.pop()} alt={component.title} />
         <MenuProvider id="menu_id" className="menu-provider">
           <div className="comments-wrapper">
@@ -56,7 +89,7 @@ class ProjectOverlay extends Component {
         </MenuProvider>
       </div>
 
-      <AddCommentMenu onClick={({ event, props })=> console.log('MenuClick', { event, props })} onSubmit={this.handleAddComment} />
+      <AddCommentMenu comment={comment} onTextChange={this.handleAddCommentText} onSubmit={this.handleAddComment} />
     </div>);
   }
 }
@@ -81,12 +114,14 @@ const ProjectViewCommentMarker = (props)=> {
 const AddCommentMenu = (props)=> {
   console.log('AddCommentMenu()', { props });
 
+  const { comment } = props;
+
   return (<Menu id="menu_id" className="add-comment-menu">
     <div className="form-wrapper">
-      <input type="text" className="comment-txt" placeholder="Add Comment" autoFocus />
+      <input type="text" className="comment-txt" placeholder="Add Comment" value={comment.text} onChange={(event)=> props.onTextChange(event.target.value)} autoFocus />
     </div>
     <div className="button-wrapper button-wrapper-row">
-      <button onClick={props.onSubmit}>Submit</button>
+      <button disabled={(comment.text.length === 0)} onClick={props.onSubmit}>Submit</button>
       <button className="cancel-button">Cancel</button>
     </div>
   </Menu>);
@@ -95,8 +130,9 @@ const AddCommentMenu = (props)=> {
 
 const mapDispatchToProps = (dispatch)=> {
   return ({
+    makeComment  : (payload)=> dispatch(makeComment(payload)),
     setComment   : (payload)=> dispatch(setComment(payload)),
-    setComponent : (payload)=> dispatch(setComponent(payload)),
+    setComponent : (payload)=> dispatch(setComponent(payload))
   });
 };
 
