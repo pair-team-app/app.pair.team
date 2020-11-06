4c4edf730f4ffcd1ca7de583dce1270be0989cfb
mtholcombe <matt@pairurl.com>
2020-11-06 02:23:39 -0500

    first component test for App

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,15 +1,50 @@
 
-
-
 import React from 'react';
+import cookie from 'react-cookies';
 import ReactDOM from 'react-dom';
-import App from './';
+import { Provider } from 'react-redux';
+import { Route } from 'react-router';
+import { ConnectedRouter } from 'connected-react-router'
 
-it('renders without crashing', ()=> {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from './App';
+import store, { history } from '../../redux/store';
+
+
+const resolver = (data)=> {
+  try {
+    expect(data).eql(!null);
+    done();
+
+  } catch (error) {
+    done(error);
+  }
+};
+
+
+
+test('stores login cookie', async(done)=> {
+  if (typeof cookie.load('cookies') === 'undefined') {
+  	await cookie.save('cookies', '0', { path : '/', sameSite : false });
+    resolver(true);
+  }
+
+  expect(true)
 });
 
 
+it('renders without crashing', async(done)=> {
+  // const div = document.getElementById('root');
+  const div = document.createElement('div');
 
+  ReactDOM.render(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <Route path="/" render={(routeProps)=> <App { ...routeProps } />} />
+      </ConnectedRouter>
+    </Provider>,
+	  div
+  );
+  ReactDOM.unmountComponentAtNode(div);
+
+  done();
+});
